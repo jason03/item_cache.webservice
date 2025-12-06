c0d0c8840b6af43d68c04e41df55de512df7e0d2
Jason Harris <jasonhh@mac.com>
2025-12-06 00:37:17 +0000

    wip. fixed item serialisation. no longer includes class info in returned deserialised object from cache

--- src/main/kotlin/net/zeotrope/item/configurer/RedisCacheConfig.kt
+++ src/main/kotlin/net/zeotrope/item/configurer/RedisCacheConfig.kt
@@ -1,10 +1,13 @@
 package net.zeotrope.item.configurer
 
+import com.fasterxml.jackson.annotation.JsonInclude.Include
 import com.fasterxml.jackson.annotation.JsonTypeInfo
+import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import net.zeotrope.item.domain.Item
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -13,6 +16,7 @@ import org.springframework.data.redis.core.ReactiveRedisTemplate
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
 import org.springframework.data.redis.serializer.RedisSerializationContext
+import java.text.SimpleDateFormat
 
 @Configuration
 @EnableRedisRepositories
@@ -21,12 +25,18 @@ class RedisCacheConfig {
     @Bean
     fun reactiveRedisTemplate(factory: ReactiveRedisConnectionFactory): ReactiveRedisTemplate<String, Item> {
         val objectMapper = jacksonObjectMapper()
-        objectMapper.registerModule(JavaTimeModule())
-        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+            .findAndRegisterModules()
+            .setSerializationInclusion(Include.ALWAYS)
+            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+            .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+            .setDateFormat(SimpleDateFormat("YYYY-MM-dd"))
+            .configure(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_USING_DEFAULT_VALUE, true)
+            .registerModule(JavaTimeModule())
+            .registerKotlinModule()
         objectMapper.activateDefaultTyping(
             objectMapper.polymorphicTypeValidator,
             ObjectMapper.DefaultTyping.NON_FINAL,
-            JsonTypeInfo.As.PROPERTY
+            JsonTypeInfo.As.WRAPPER_OBJECT
         )
         val serializer = GenericJackson2JsonRedisSerializer(objectMapper).configure {}
         val context = RedisSerializationContext
