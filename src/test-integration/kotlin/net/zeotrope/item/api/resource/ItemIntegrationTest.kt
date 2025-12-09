83f4411d3e1a132344180c677481ca3cd9cfb5d7
Jason Harris <jasonhh@mac.com>
2025-12-09 00:23:26 +0000

    update int test so context doesn't need to reload before each test

--- src/test-integration/kotlin/net/zeotrope/item/api/resource/ItemIntegrationTest.kt
+++ src/test-integration/kotlin/net/zeotrope/item/api/resource/ItemIntegrationTest.kt
@@ -7,8 +7,10 @@ import net.zeotrope.item.domain.ItemStatus
 import net.zeotrope.item.model.ItemDto
 import net.zeotrope.item.util.TestServiceContainers
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.MethodOrderer
 import org.junit.jupiter.api.Order
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestMethodOrder
 import org.junit.jupiter.api.assertAll
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
@@ -23,7 +25,8 @@ import java.time.LocalDateTime
 
 @Import(TestcontainersConfiguration::class)
 @ActiveProfiles("test")
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation::class)
 @AutoConfigureWebTestClient
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ItemIntegrationTest : TestServiceContainers() {
@@ -31,7 +34,7 @@ class ItemIntegrationTest : TestServiceContainers() {
     @Autowired
     private lateinit var webTestClient: WebTestClient
 
-    @Order(1)
+    @Order(value = 1)
     @Test
     fun `should return 200 and when get all items`() = runTest {
         // given
@@ -51,7 +54,7 @@ class ItemIntegrationTest : TestServiceContainers() {
             }
     }
 
-    @Order(2)
+    @Order(value = 2)
     @Test
     fun `should return 200 and get item by id`() {
         // given
@@ -80,7 +83,7 @@ class ItemIntegrationTest : TestServiceContainers() {
             .verifyComplete()
     }
 
-    @Order(3)
+    @Order(value = 3)
     @Test
     fun `should return 404 when get item by invalid id`() {
         // given
@@ -97,7 +100,7 @@ class ItemIntegrationTest : TestServiceContainers() {
             .isEqualTo("Item not found for request: /api/v1/items/$itemId")
     }
 
-    @Order(4)
+    @Order(value = 4)
     @Test
     fun `should create an item into the test data db and return 201 with id 7`() {
         // given
@@ -118,40 +121,28 @@ class ItemIntegrationTest : TestServiceContainers() {
             .jsonPath("$.id").isEqualTo(7)
     }
 
-    @Order(5)
+    @Order(value = 5)
     @Test
     fun `should update an item into the test data db and return 204`() {
         // given
-        val item = ItemDto(
-            name = "New Test Item Two",
-            status = ItemStatus.CURRENT,
-            description = "New Test Item Two Description"
-        )
-        val updatedDescription = "New Test Item Two with a longer description"
+        val updatedDescription = "Test Item with a longer description"
         val updatedItem = ItemDto(
-            name = "New Test Item Two",
+            name = "Test Item With Updates",
             status = ItemStatus.CURRENT,
             description = updatedDescription
         )
 
         // when
         // then
-        webTestClient.post()
-            .uri("/api/v1/items")
-            .bodyValue(item)
-            .exchange()
-            .expectStatus().isCreated
-            .expectBody()
-            .jsonPath("$.id").isEqualTo(7)
         webTestClient.put()
-            .uri("/api/v1/items/7")
+            .uri("/api/v1/items/1")
             .bodyValue(updatedItem)
             .exchange()
             .expectStatus().isNoContent
             .expectBody().isEmpty
     }
 
-    @Order(6)
+    @Order(value = 6)
     @Test
     fun `should update an item's status to DISCONTINUED and set the discontinuedAt date`() {
         // given
@@ -169,7 +160,7 @@ class ItemIntegrationTest : TestServiceContainers() {
             .expectBody().isEmpty
     }
 
-    @Order(7)
+    @Order(value = 7)
     @Test
     fun `should delete an item with an invalid id and return 204`() {
         // given
@@ -182,7 +173,7 @@ class ItemIntegrationTest : TestServiceContainers() {
             .expectBody().isEmpty
     }
 
-    @Order(7)
+    @Order(value = 8)
     @Test
     fun `should delete an item with an valid id and return 204 and confirm with get returning a 404`() {
         // given
